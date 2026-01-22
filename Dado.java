23cb808765137e6ffcc4cc78213895b77d372262
Cappe08 <cesarecappellaro8@gmail.com>
2026-01-22 20:41:37 +0100

    Update Dado.java

--- Dado.java
+++ Dado.java
@@ -1,21 +1,22 @@
 import java.util.Random;
 
-public class Dado {
+public class Dado{
 
     private int numeroFacce;
 
     // Costruttore
-    public Dado(int numeroFacce) {
-        this.numeroFacce = numeroFacce;
+    public Dado(int numeroFacce){
+        this.numeroFacce=numeroFacce;
     }
 
-    public int getNumeroFacce() {
+    public int getNumeroFacce(){
         return numeroFacce;
     }
 
     // Lancia il dado e restituisce un numero tra 1 e numeroFacce
-    public int lancia(int numeroFacce) {
-        Random r = new Random();
-        return r.nextInt(numeroFacce) + 1;
+    public int lancia(int numeroFacce){
+        Random r=new Random();
+        return r.nextInt(numeroFacce)+1;
     }
 }
+
