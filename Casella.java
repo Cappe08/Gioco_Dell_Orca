6f171c519cb986b3d4ea8c1673e5f2e7e22e08ec
Cappe08 <cesarecappellaro8@gmail.com>
2026-01-22 20:41:04 +0100

    Casella

--- Casella.java
+++ Casella.java
@@ -4,105 +4,118 @@ public class Casella {
     private String titolo;
     private Casella precedente;
     private Casella successiva;
-    private Domanda1 domanda;
+    private Domanda domanda;
     private Giocatore[] giocatoriPresenti;
     private int numGiocatori;
 
     // Costruttore
-    public Casella(int id, String titolo, Domanda1 domanda, int maxGiocatori) {
-        this.id = id;
-        this.titolo = titolo;
-        this.domanda = domanda;
-        this.giocatoriPresenti = new Giocatore[maxGiocatori];
-        this.numGiocatori = 0;
+    public Casella(int id, String titolo, Domanda domanda, int maxGiocatori) {
+        this.id=id;
+        this.titolo=titolo;
+        this.domanda=domanda;
+        this.giocatoriPresenti=new Giocatore[maxGiocatori];
+        this.numGiocatori=0;
     }
 
     // Getter e setter
-    public int getId() {
+    public int getId(){
         return id;
     }
 
-    public String getTitolo() {
+    public String getTitolo(){
         return titolo;
     }
 
-    public Casella getPrecedente() {
+    public Casella getPrecedente(){
         return precedente;
     }
 
-    public void setPrecedente(Casella precedente) {
-        this.precedente = precedente;
+    public void setPrecedente(Casella precedente){
+        this.precedente=precedente;
     }
 
-    public Casella getSuccessiva() {
+    public Casella getSuccessiva(){
         return successiva;
     }
 
-    public void setSuccessiva(Casella successiva) {
-        this.successiva = successiva;
+    public void setSuccessiva(Casella successiva){
+        this.successiva=successiva;
     }
 
     // Numero di giocatori presenti
-    public int getNumGiocatori() {
-        int count = 0;
-        for (Giocatore g : giocatoriPresenti) {
-            if (g != null) count++;
+    public int getNumGiocatori(){
+    int count=0;
+
+    for(int i=0;i<giocatoriPresenti.length;i++) {
+        if(giocatoriPresenti[i]!=null) {
+            count=count+1;
         }
-        return count;
+    }
+    return count;
     }
 
     // Aggiunge un giocatore
-    public void aggiungiGiocatore(Giocatore g) {
-        if (numGiocatori < giocatoriPresenti.length) {
-            giocatoriPresenti[numGiocatori++] = g;
+    public void aggiungiGiocatore(Giocatore g){
+        if(numGiocatori<giocatoriPresenti.length){
+            giocatoriPresenti[numGiocatori]=g;
+            numGiocatori=numGiocatori+1;
             g.setCasellaCorrente(this);
-        } else {
-            System.out.println("Casella piena, impossibile aggiungere " + g.getNome());
+        }else{
+        System.out.println("Casella piena, impossibile aggiungere " + g.getNome());
         }
     }
 
     // Rimuove un giocatore
-    public void rimuoviGiocatore(Giocatore g) {
-        for (int i = 0; i < numGiocatori; i++) {
-            if (giocatoriPresenti[i] == g) {
-                for (int j = i; j < numGiocatori - 1; j++) {
-                    giocatoriPresenti[j] = giocatoriPresenti[j + 1];
+    public void rimuoviGiocatore(Giocatore g){
+        for(int i=0;i<numGiocatori;i++){
+            if(giocatoriPresenti[i]==g){
+                for(int j=i;j<numGiocatori-1;j++){
+                    giocatoriPresenti[j]=giocatoriPresenti[j+1];
                 }
-                giocatoriPresenti[--numGiocatori] = null;
+                giocatoriPresenti[numGiocatori-1]=null;
                 break;
             }
         }
     }
 
     // Restituisce l'altro giocatore presente sulla casella (per la lotta)
-    public Giocatore getAltroGiocatore(Giocatore g) {
-        for (Giocatore giocatore : giocatoriPresenti) {
-            if (giocatore != null && giocatore != g) {
-                return giocatore;
+    public Giocatore getAltroGiocatore(Giocatore g){
+        for(int i=0;i<giocatoriPresenti.length;i++){
+            if(giocatoriPresenti[i]!=null){
+                if(giocatoriPresenti[i]!=g){
+                    return giocatoriPresenti[i]; // trovato un altro giocatore
+                }else{
+                    // è lo stesso giocatore, non facciamo nulla
+                }
+            }else{
+            // posizione vuota, non facciamo nulla
             }
         }
-        return null; // nessun altro giocatore
+        // se non abbiamo trovato nessun altro giocatore
+        return null;
     }
 
+
     // Attiva la domanda della casella
-    public void attivaDomanda(Giocatore g) {
-        if (domanda == null) {
+    public void attivaDomanda(Giocatore g){
+        if(domanda==null){
             System.out.println("Nessuna domanda in questa casella.");
             return;
         }
 
-        System.out.println("\nCasella " + (id + 1));
+        System.out.println("\n Casella "+(id+1));
         System.out.println(domanda);
 
         System.out.print("Inserisci la risposta: ");
         String risposta = Leggi.unoString();
 
-        if (domanda.valutaRisposta(risposta)) {
-            System.out.println("Risposta corretta! +" + domanda.getPunti());
+        if(domanda.valutaRisposta(risposta)) {
+            System.out.println("Risposta corretta! +"+domanda.getPunti());
             g.riceviPunti(domanda.getPunti());
-        } else {
+        }else{
             System.out.println("Risposta sbagliata! Punti dimezzati.");
             g.dimezzaPunteggio();
         }
     }
 }
+
