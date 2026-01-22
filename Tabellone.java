8c00e674608a676c2e03fa8a27ba4bca62ac6b79
Cappe08 <cesarecappellaro8@gmail.com>
2026-01-22 20:42:08 +0100

    Update Tabellone.java

--- Tabellone.java
+++ Tabellone.java
@@ -3,54 +3,64 @@ public class Tabellone {
     private int numeroCaselle;
     private Casella[] listaCaselle;
 
-    public Tabellone(int numeroCaselle, Domandiere domandiere) {
-        this.numeroCaselle = numeroCaselle;
-        this.listaCaselle = new Casella[numeroCaselle];
+    public Tabellone(int numeroCaselle, Domandiere domandiere){
+        this.numeroCaselle=numeroCaselle;
+        this.listaCaselle=new Casella[numeroCaselle];
 
         costruisciTabellone(domandiere);
     }
+    public void costruisciTabellone(Domandiere domandiere){
 
-    public void costruisciTabellone(Domandiere domandiere) {
+    Domanda[] domande=domandiere.getDomande();
 
-        Domanda1[] domande = domandiere.getDomande();
-
-        for (int i = 0; i < numeroCaselle; i++) {
-            Domanda1 d = domandiere.scegliDomanda(domande);
-
-            listaCaselle[i] = new Casella(
-                i,
-                "Casella " + i,
-                d,
-                4
-            );
-        }
-
-        for (int i = 0; i < numeroCaselle; i++) {
-            if (i > 0) listaCaselle[i].setPrecedente(listaCaselle[i - 1]);
-            if (i < numeroCaselle - 1) listaCaselle[i].setSuccessiva(listaCaselle[i + 1]);
-        }
+    // creo le caselle
+    for(int i=0;i<numeroCaselle;i=i+1){
+        Domanda d=domandiere.scegliDomanda(domande);
+        listaCaselle[i]=new Casella(
+            i,
+            "Casella"+i,
+            d,
+            5
+        );
     }
 
-    public void spostaPedina(Giocatore g, int passi) {
+    // collego le caselle
+    for(int i=0;i<numeroCaselle;i=i+1){
+        if(i>0){
+            listaCaselle[i].setPrecedente(listaCaselle[i-1]);
+        }else{
+            // prima casella
+        }
+        if(i<numeroCaselle-1){
+            listaCaselle[i].setSuccessiva(listaCaselle[i+1]);
+        }else{
+            // ultima casella
+        }
+    }
+    }
 
-        Casella attuale = g.getCasellaCorrente();
-        Casella destinazione = spostaCasella(attuale, passi);
+
+    public void spostaPedina(Giocatore g,int passi){
+
+        Casella attuale=g.getCasellaCorrente();
+        Casella destinazione=spostaCasella(attuale,passi);
 
         attuale.rimuoviGiocatore(g);
         destinazione.aggiungiGiocatore(g);
         destinazione.attivaDomanda(g);
     }
 
-    public Casella spostaCasella(Casella c, int passi) {
-        Casella corrente = c;
+    public Casella spostaCasella(Casella c,int passi){
+        Casella corrente=c;
 
-        for (int i = 0; i < passi && corrente.getSuccessiva() != null; i++) {
-            corrente = corrente.getSuccessiva();
+        for(int i=0;i<passi && corrente.getSuccessiva()!=null;i++){
+            corrente=corrente.getSuccessiva();
         }
         return corrente;
     }
 
-    public Casella getCasella(int indice) {
+    public Casella getCasella(int indice){
         return listaCaselle[indice];
     }
 }
+
