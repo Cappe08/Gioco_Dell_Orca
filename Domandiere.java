b8ca4ec0364b3464335801ff0551b747b30f0d95
Cappe08 <cesarecappellaro8@gmail.com>
2026-01-25 17:16:41 +0100

    Update Domandiere.java

--- Domandiere.java
+++ Domandiere.java
@@ -1,57 +1,17 @@
-public class Domandiere {
+public class Domandiere{
     private Domanda[] domande;
-    private String percorsoFileDomande;
 
-    public Domandiere(Domanda[] domande, String percorsoFileDomande){
-        this.domande = domande;
-        this.percorsoFileDomande = percorsoFileDomande;
+    public Domandiere(Domanda[] domande) {
+        this.domande=domande;
     }
 
-    public String getPercorsoFileDomande(){
-        return percorsoFileDomande;
-    }
-
-    public void setPercorsoFileDomande(String percorsoFileDomande){
-        this.percorsoFileDomande = percorsoFileDomande;
-    }
-
-    public Domanda[] getDomande(){
-        return domande;
-    }
-
-    public void setDomande(Domanda[] domande){
-        this.domande = domande;
-    }
-
-    //metodo per scegliere una domanda casuale dall'array di domande
-    public Domanda scegliDomanda(Domanda[] domande){
-        if(domande.length == 0 || domande == null){
+    public Domanda scegli(Giocatore g){
+        if(domande==null || domande.length==0){
             return null;
         }
 
-        int indice = (int)(Math.random() * domande.length);
+        int indice=(int)(Math.random()*domande.length);
         return domande[indice];
     }
-
-    // Legge il file e restituisce il contenuto come String
-    public String leggiFile(String percorsoFile) {
-        String contenuto = "";
-
-        try {
-            java.io.BufferedReader br = new java.io.BufferedReader(new java.io.FileReader(percorsoFile));
-
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                contenuto += linea + "\n";
-            }
-
-            br.close();
-        } catch (java.io.IOException e) {
-            System.out.println("Errore nella lettura del file");
-        }
-
-        return contenuto;
-    }
-
 }
 
